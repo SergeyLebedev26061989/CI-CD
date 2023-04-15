1968e0619b512be94069209902c5ea805e43faf7
Sergey <seryoga-lebedev@yandex.ru>
2023-04-15 16:25:47 +0300

    add HW ci/cd

---
+++ api_with_restrictions/advertisements/tests.py
@@ -0,0 +1,24 @@
+from unittest import TestCase
+from rest_framework.test import APIClient
+from advertisements.views import AdvertisementViewSet
+
+
+class TestView(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def _require_login(self):
+        self.client.login(username='testuser', password='1')
+
+    def test_view_401(self):
+        url = 'http://194.58.97.187/api/'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 401)
+
+    def test_view_200(self):
+        # self.client._require_login()
+        request = self.client.get('http://194.58.97.187/api/')
+        view = views.AdvertisementViewSet.as_view()
+        response = view(request)
+        self.assertEqual(response.status_code, 200)
+
---
+++ api_with_restrictions/advertisements/views.py
@@ -0,0 +1,39 @@
+from django_filters.rest_framework import DjangoFilterBackend
+# from rest_framework.filters import SearchFilter, OrderingFilter
+# from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+
+from .filters import AdvertisementFilter
+from .models import Advertisement
+from .serializers import AdvertisementSerializer
+
+from .permissions import IsOwnerOrReadOnly
+
+
+class AdvertisementViewSet(ModelViewSet):
+    """ViewSet для объявлений."""
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['creator', 'status']
+
+    # search_fields = ['status', 'title']
+    # ordering_fields = ['id', 'username', 'creator', 'status', 'created_at']
+
+    # TODO: настройте ViewSet, укажите атрибуты для кверисета,
+    #   сериализаторов и фильтров
+
+    def get_permissions(self):
+        """Получение прав для действий."""
+        if self.action in ["create", "update", "partial_update", "destroy"]:
+            return [IsOwnerOrReadOnly()]
+        return []
+
+    def list(self, request):
+        queryset = Advertisement.objects.all()
+        queryset = AdvertisementFilter(data=request.GET, queryset=queryset, request=request).qs # noqa
+        serializer = AdvertisementSerializer(queryset, many=True)
+        return Response(serializer.data)
