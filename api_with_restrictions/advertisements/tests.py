88fa90796de69511bf290341e8c69ea885242c54
Sergey <seryoga-lebedev@yandex.ru>
2023-06-07 18:36:43 +0300

    add files HW_07.06.2023

--- api_with_restrictions/advertisements/tests.py
+++ api_with_restrictions/advertisements/tests.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 from rest_framework.test import APIClient
-from advertisements.views import AdvertisementViewSet
+# from advertisements.views import AdvertisementViewSet
 
 
 class TestView(TestCase):
@@ -17,8 +17,8 @@ class TestView(TestCase):
 
     def test_view_200(self):
         # self.client._require_login()
+        client = APIClient()
         request = self.client.get('http://194.58.97.187/api/')
         view = views.AdvertisementViewSet.as_view()
         response = view(request)
         self.assertEqual(response.status_code, 200)
-
